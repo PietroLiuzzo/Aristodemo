3de85074ab84a070048f7f4f4107231e4eefbcb8
Pietro Liuzzo <pietro.liuzzo@gmail.com>
2022-06-29 10:26:56 +0200

    Update openseadragon81.js

--- js/openseadragon81.js
+++ js/openseadragon81.js
@@ -5,6 +5,6 @@
                            preserveViewport: true,
                            visibilityRatio:    1,
                            minZoomLevel:       1,
-                           defaultZoomLevel:   1,    sequenceMode:       true,tileSources:   ["http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f171/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f172/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f173/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f174/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f175/info.json" ]
+                           defaultZoomLevel:   1,    sequenceMode:       true,tileSources:   ["https://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f171/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f172/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f173/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f174/info.json","http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f175/info.json" ]
                            });
                         
