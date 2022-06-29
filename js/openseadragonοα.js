036e0b74220e575c98962b11741b58bc2075b386
Pietro Liuzzo <pietro.liuzzo@gmail.com>
2022-06-29 10:30:58 +0200

    Update openseadragonοα.js

--- js/openseadragonοα.js
+++ js/openseadragonοα.js
@@ -5,6 +5,6 @@
                            preserveViewport: true,
                            visibilityRatio:    1,
                            minZoomLevel:       1,
-                           defaultZoomLevel:   1,tileSources:   ["http://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f184/info.json" ]
+                           defaultZoomLevel:   1,tileSources:   ["https://gallica.bnf.fr/iiif/ark:/12148/btv1b8593585j/f184/info.json" ]
                            });
                         
